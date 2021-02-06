1afb9420bfcb28e9f13441063304a6c62bd11eec
Prachi <prachiprachi15031999@gmail.com>
2021-02-07 00:10:42 +0530

    fix : listMembers

--- listMembers.py
+++ listMembers.py
@@ -1,7 +1,7 @@
 from client import client
 from logger import infoLogger
 from services.user import submit_user_details
-async def listExixtingMembers():
+async def listExistingMembers():
     for member in client.get_all_members():
         if not member.bot:
             infoLogger.info('database call to create '+str(member.name)+' with id '+ str(member.id)+' is sent.')
